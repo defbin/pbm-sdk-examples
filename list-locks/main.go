48c8a2fa468bdb5628aa11c00cb3f0100ae98905
Dmytro Zghoba <dmytro.zghoba@gmail.com>
2024-06-27 17:40:44 +0200

    add list-locks example

---
+++ list-locks/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	sdk "github.com/percona/percona-backup-mongodb/v2"
+)
+
+const DefaultMongoURI = "mongodb://localhost:27017"
+
+func main() {
+	ctx := context.Background()
+
+	uri := os.Getenv("DATABASE_URI")
+	if uri == "" {
+		uri = DefaultMongoURI
+	}
+
+	pbm, err := sdk.NewClient(ctx, uri)
+	if err != nil {
+		log.Fatalf("new sdk client: %v", err)
+	}
+	defer func() {
+		err := pbm.Close(context.Background())
+		if err != nil {
+			log.Printf("close sdk client: %v", err)
+		}
+	}()
+
+	locks, err := pbm.OpLocks(ctx)
+	if err != nil {
+		log.Fatalf("oplock: %v", err)
+	}
+
+	if len(locks) == 0 {
+		fmt.Println("<noop>")
+		return
+	}
+
+	for _, lock := range locks {
+		fmt.Printf("> cmd: %s; rs: %q; node: %q",
+			lock.Cmd,
+			lock.Replset,
+			lock.Node)
+		if err := lock.Err(); err != nil {
+			if errors.Is(err, sdk.ErrStaleHearbeat) {
+				fmt.Printf(" stuck: " + err.Error())
+			} else {
+				fmt.Printf(" ERROR: " + err.Error())
+			}
+		}
+		fmt.Println()
+	}
+}
